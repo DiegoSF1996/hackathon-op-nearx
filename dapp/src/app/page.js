7d86612284784fed0150adc9395b4989ef50c46b
Diego Figueiredo <diegosf.dsf@gmail.com>
2024-03-20 19:40:15 -0300

    Criando pag de login e cadastro

--- dapp/src/app/page.js
+++ dapp/src/app/page.js
@@ -2,6 +2,8 @@
 import { useState } from 'react';
 import { login } from '@/services/Web3Service'
 import { InputPrecoMoeda, Teste } from '@/components/input'
+import Image from 'next/image';
+
 
 export default function Home() {
   const [connectedAccount, setConnectedAccount] = useState('null');
@@ -32,6 +34,29 @@ export default function Home() {
   }
   return (
     <main>
+
+      <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20"></div>
+        <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"></div>
+        <div className="mx-auto max-w-2xl lg:max-w-4xl">
+          {/* <img className="mx-auto h-12" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" alt="" /> */}
+          <figure className="mt-10">
+            <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+              <p>“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.”</p>
+            </blockquote>
+            <figcaption className="mt-10">
+              <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+                <button onClick={()=>btnLogin()} class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                  Acesse com sua metamask
+                  <img className="mx-auto h-10 w-10 rounded-full" src="https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg" alt="" />
+                </button>
+              </blockquote>
+             
+            </figcaption>
+          </figure>
+        </div>
+      </section>
+      {/* <button onClick={()=>btnLogin()}>sdsd</button>
       <div className="bg-gray-100">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
@@ -80,7 +105,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <Teste />
+      <Teste /> */}
     </main>
 
     // https://tailwindcss.com/docs/padding
--- dapp/src/services/Web3Service.js
+++ dapp/src/services/Web3Service.js
@@ -1,11 +1,37 @@
 import { Web3 } from 'web3';
+import ABI from "./ABI.json"
 
-export async function login(){
-    if(!window.ethereum) throw new Error("MetaMask não encontrada");
+const CONTRACT_ADDRESS = "";
+
+export async function login() {
+    if (!window.ethereum) throw new Error("MetaMask não encontrada");
     const web3 = new Web3(window.ethereum);
     const accounts = await web3.eth.requestAccounts();
-    if(!accounts || ! accounts.length) throw new Error("MetaMask sem permissão");
-    localStorage.setItem("wallet",accounts[0]);
+    if (!accounts || !accounts.length) throw new Error("MetaMask sem permissão");
+    localStorage.setItem("wallet", accounts[0]);
     return accounts[0];
-    
+
+}
+
+export async function estaLogado() {
+    const wallet = localStorage.getItem("wallet");
+    if (!wallet) throw new Error("Não autorizado");
+    return wallet;
+}
+
+async function obterContrato() {
+    const wallet = localStorage.getItem("wallet");
+    if (!wallet) throw new Error("Não autorizado");
+
+    const web3 = new Web3(window.ethereum);
+    return new web3.eth.Contract(ABI, CONTRACT_ADDRESS, { from: wallet });
+}
+
+export async function cadastrar(dados) {
+    const wallet = localStorage.getItem("wallet");
+    if (!wallet) throw new Error("Não autorizado");
+
+    const web3 = new Web3(window.ethereum);
+    const contract = await obterContrato();
+    return contract.methods.cadastrar().call();
 }
