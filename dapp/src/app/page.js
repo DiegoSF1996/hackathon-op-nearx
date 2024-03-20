9b6b2316fb4884f4207b7be0c67421183f453c25
Diego Figueiredo <diegosf.dsf@gmail.com>
2024-03-20 12:21:21 -0300

    Ajustando docker e criando componente de exemplo

--- dapp/src/app/page.js
+++ dapp/src/app/page.js
@@ -1,14 +1,14 @@
 "use client";
-import Image from "next/image";
 import { useState } from 'react';
-import {login} from '@/services/Web3Service'
+import { login } from '@/services/Web3Service'
+import { InputPrecoMoeda, Teste } from '@/components/input'
 
 export default function Home() {
   const [connectedAccount, setConnectedAccount] = useState('null');
   const [message, setMessage] = useState('null');
-  
-  function btnLogin (){
-    login().then(account => setMessage(account)).catch(err => { 
+
+  function btnLogin() {
+    login().then(account => setMessage(account)).catch(err => {
       console.log(err);
       setMessage(err);
     })
@@ -32,8 +32,58 @@ export default function Home() {
   }
   return (
     <main>
-       <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/512px-MetaMask_Fox.svg.png' alt="Logo" />
-      <button onClick={() => btnLogin()}>Conectar</button>
+      <div className="bg-gray-100">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+            <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+
+            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+              <div className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." className="h-full w-full object-cover object-center" />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href="#">
+                    <span className="absolute inset-0"></span>
+                    Desk and Office
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">Work from home accessories</p>
+              </div>
+              <div className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." className="h-full w-full object-cover object-center" />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href="#">
+                    <span className="absolute inset-0"></span>
+                    Self-Improvement
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">Journals and note-taking</p>
+              </div>
+              <div className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg" alt="Collection of four insulated travel bottles on wooden shelf." className="h-full w-full object-cover object-center" />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href="#">
+                    <span className="absolute inset-0"></span>
+                    Travel
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">Daily commute essentials </p>
+                <InputPrecoMoeda valor={10} description="Blockchain Developer" />
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Teste />
     </main>
+
+    // https://tailwindcss.com/docs/padding
+    // https://tailwindui.com/components/preview
   );
 }
