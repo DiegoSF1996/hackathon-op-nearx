8a366b3d1a32283346c490e5800e71fa3a96aaaa
Diego Figueiredo <diegosf.dsf@gmail.com>
2024-03-21 20:10:09 -0300

    Limpando código e add navbar

--- dapp/src/app/projetos/page.js
+++ dapp/src/app/projetos/page.js
@@ -1,103 +1,82 @@
 "use client";
 import { useState } from 'react';
-import { login } from '@/services/Web3Service'
-import { InputPrecoMoeda, Teste } from '@/components/input'
-import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { Navbar } from '@/components/navbar';
 
 
 export default function Home() {
-  const [connectedAccount, setConnectedAccount] = useState('null');
   const [message, setMessage] = useState('null');
 
+  const { push } = useRouter();
 
-  function btnLogin() {
-    login().then(account => setMessage(account)).catch(err => {
-      console.log(err);
-      setMessage(err);
-    })
-  }
-  async function connectMetamask() {
-    //check metamask is installed
-    if (window.ethereum) {
-      // instantiate Web3 with the injected provider
-      const web3 = new Web3(window.ethereum);
-
-      //request user to connect accounts (Metamask will prompt)
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-      //get the connected accounts
-      const accounts = await web3.eth.getAccounts();
-      //show the first connected account in the react page
-      setConnectedAccount(accounts[0]);
-    } else {
-      alert('Please download metamask');
-    }
-  }
   return (
     <main>
 
-      <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Novo
-      </button>
+      <Navbar />
+      <div className='container'>
+        <button onClick={() => push('/projetos/novo')} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Novo
+        </button>
+      </div>
 
-      <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Product name
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Color
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Category
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Price
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                 Apple MacBook Pro 17"
               </th>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 Silver
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 Laptop
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $2999
               </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                 Microsoft Surface Pro
               </th>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 White
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 Laptop PC
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $1999
               </td>
             </tr>
-            <tr class="bg-white dark:bg-gray-800">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <tr className="bg-white dark:bg-gray-800">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                 Magic Mouse 2
               </th>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 Black
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 Accessories
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 $99
               </td>
             </tr>
