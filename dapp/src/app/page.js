867c963a2271ca88705682a39bc71ce1f7b67a87
Diego Figueiredo <diegosf.dsf@gmail.com>
2024-03-21 20:09:30 -0300

    Fazendo integração backend e front tela de login

--- dapp/src/app/page.js
+++ dapp/src/app/page.js
@@ -1,40 +1,49 @@
 "use client";
-import { useState } from 'react';
-import { login } from '@/services/Web3Service'
-import { InputPrecoMoeda, Teste } from '@/components/input'
-import Image from 'next/image';
-
+import { useState, useEffect } from 'react';
+import { login, checarUsuarioJaCadastrado, estaLogado } from '@/services/Web3Service'
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const [connectedAccount, setConnectedAccount] = useState('null');
   const [message, setMessage] = useState('null');
+  const { push } = useRouter();
+
+  function checarLogado() {
+    estaLogado()
+    .then(() => {
+      checarUsuarioJaCadastrado().then(
+        dados => {
+          push('/projetos/novo')
+        }
+      ).catch()
+    })
+    .catch();
+  }
+
 
   function btnLogin() {
-    login().then(account => setMessage(account)).catch(err => {
+    
+    login().then(account => {
+      setConnectedAccount(account);
+      checarUsuarioJaCadastrado().then(
+        dados => {
+          if(dados == true ){
+            push('/projetos/novo')
+          } else {
+            push('/cadastro')
+          }
+
+        }
+      ).catch()
+    })
+    .catch(err => {
       console.log(err);
       setMessage(err);
     })
   }
-  async function connectMetamask() {
-    //check metamask is installed
-    if (window.ethereum) {
-      // instantiate Web3 with the injected provider
-      const web3 = new Web3(window.ethereum);
 
-      //request user to connect accounts (Metamask will prompt)
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-      //get the connected accounts
-      const accounts = await web3.eth.getAccounts();
-      //show the first connected account in the react page
-      setConnectedAccount(accounts[0]);
-    } else {
-      alert('Please download metamask');
-    }
-  }
   return (
     <main>
-
       <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20"></div>
         <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"></div>
@@ -42,70 +51,21 @@ export default function Home() {
           {/* <img className="mx-auto h-12" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" alt="" /> */}
           <figure className="mt-10">
             <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-              <p>“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.”</p>
+              <p>O <b>Brasil é a terra da inovação</b>, a <b></b>criatividade está no sangue do brasileiro vamos <b>construir, inovar</b> e fazer nossa criatividade crescer ainda mais, conecte sua <b>metamask</b> para participar do futuro!</p>
             </blockquote>
             <figcaption className="mt-10">
               <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-                <button onClick={()=>btnLogin()} class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                <button onClick={() => btnLogin()} class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                   Acesse com sua metamask
                   <img className="mx-auto h-10 w-10 rounded-full" src="https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg" alt="" />
                 </button>
               </blockquote>
-             
+
             </figcaption>
           </figure>
         </div>
       </section>
-      {/* <button onClick={()=>btnLogin()}>sdsd</button>
-      <div className="bg-gray-100">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-            <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
-
-            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-              <div className="group relative">
-                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                  <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." className="h-full w-full object-cover object-center" />
-                </div>
-                <h3 className="mt-6 text-sm text-gray-500">
-                  <a href="#">
-                    <span className="absolute inset-0"></span>
-                    Desk and Office
-                  </a>
-                </h3>
-                <p className="text-base font-semibold text-gray-900">Work from home accessories</p>
-              </div>
-              <div className="group relative">
-                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                  <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." className="h-full w-full object-cover object-center" />
-                </div>
-                <h3 className="mt-6 text-sm text-gray-500">
-                  <a href="#">
-                    <span className="absolute inset-0"></span>
-                    Self-Improvement
-                  </a>
-                </h3>
-                <p className="text-base font-semibold text-gray-900">Journals and note-taking</p>
-              </div>
-              <div className="group relative">
-                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                  <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg" alt="Collection of four insulated travel bottles on wooden shelf." className="h-full w-full object-cover object-center" />
-                </div>
-                <h3 className="mt-6 text-sm text-gray-500">
-                  <a href="#">
-                    <span className="absolute inset-0"></span>
-                    Travel
-                  </a>
-                </h3>
-                <p className="text-base font-semibold text-gray-900">Daily commute essentials </p>
-                <InputPrecoMoeda valor={10} description="Blockchain Developer" />
-
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Teste /> */}
+  
     </main>
 
     // https://tailwindcss.com/docs/padding
