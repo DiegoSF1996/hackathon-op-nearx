22b6e70da5b3d079475f361e9ebcf2539d71cd9f
Diego Figueiredo <diegosf.dsf@gmail.com>
2024-03-21 21:14:48 -0300

    feature - integração listagem de projetos do usuário

--- dapp/src/app/projetos/page.js
+++ dapp/src/app/projetos/page.js
@@ -1,14 +1,19 @@
 "use client";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { Navbar } from '@/components/navbar';
+import { listarProjetosUsuario } from '@/services/Web3Service'
 
 
 export default function Home() {
   const [message, setMessage] = useState('null');
+  const [projetos, setProjetos] = useState([]);
 
   const { push } = useRouter();
 
+  useEffect(() => {
+    listarProjetosUsuario().then(dados => { setProjetos(dados); console.log(dados) })
+  }, [])
   return (
     <main>
 
@@ -18,68 +23,68 @@ export default function Home() {
           Novo
         </button>
       </div>
-
       <div className="relative overflow-x-auto">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Product name
+                Nome do Projeto
               </th>
               <th scope="col" className="px-6 py-3">
-                Color
+                Meta arrecadação
               </th>
               <th scope="col" className="px-6 py-3">
-                Category
+                Valor Arrecadado
               </th>
               <th scope="col" className="px-6 py-3">
-                Price
+                Descricão Projeto
+              </th>
+              <th scope="col" className="px-6 py-3">
+                imagem
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Site
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Situacao
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Tags
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook Pro 17"
-              </th>
-              <td className="px-6 py-4">
-                Silver
-              </td>
-              <td className="px-6 py-4">
-                Laptop
-              </td>
-              <td className="px-6 py-4">
-                $2999
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">
-                White
-              </td>
-              <td className="px-6 py-4">
-                Laptop PC
-              </td>
-              <td className="px-6 py-4">
-                $1999
-              </td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">
-                Black
-              </td>
-              <td className="px-6 py-4">
-                Accessories
-              </td>
-              <td className="px-6 py-4">
-                $99
-              </td>
-            </tr>
+            {projetos.map(projeto =>
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {projeto[0]}
+                </th>
+                <td className="px-6 py-4">
+                  {projeto[1]}
+
+                </td>
+                <td className="px-6 py-4">
+                  {projeto[2]}
+
+                </td>
+                <td className="px-6 py-4">
+                  {projeto[3]}
+                </td>
+                <td className="px-6 py-4">
+                  {projeto[4]}
+                </td>
+                <td className="px-6 py-4">
+                  {projeto[5]}
+                </td>
+                <td className="px-6 py-4">
+                  {projeto[6]}
+                </td>
+                <td className="px-6 py-4">
+                  {projeto[7]? 'Finalizado' : 'Em adamento' }
+                </td>
+              </tr>
+            )}
+
           </tbody>
         </table>
       </div>
